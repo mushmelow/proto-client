18231cbcda368198521c12ef5123a3323c91e73c
abarr <crichard>
2018-04-04 17:03:56 +0100

    Fixed navbar

--- src/components/layout/NavigationBarDisconnected.js
+++ src/components/layout/NavigationBarDisconnected.js
@@ -6,7 +6,7 @@ class NavigationBarDisconnected extends Component {
 
     render() {
         return (
-        	<div>
+        	<nav className="navbar navbar-expand-lg navbar-light bg-light">
 					  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 					    <span className="navbar-toggler-icon"></span>
 					  </button>
@@ -20,7 +20,7 @@ class NavigationBarDisconnected extends Component {
 					      </li>
 					    </ul>
 					  </div>
-				  </div>
+				  </nav>
         );
     }
 }
